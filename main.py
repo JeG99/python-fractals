3dfddaa5f02ea3a97600dba39d0cac45be3f95cf
Elias Garza <a00824494@itesm.mx>
2022-02-02 01:08:40 -0600

    cleaning

--- main.py
+++ main.py
@@ -4,14 +4,13 @@
 
 from fractal import Fractal
 
-dim = (1080, 1920) # resolucion de la imagen
+#dim = (1080, 1920) # resolucion de la imagen
+#frac = Fractal('mandelbrot', 100, dim, -3.3, 1.9, -1.5, 1.5)
 
-frac = Fractal('mandelbrot', 100, dim, -3.3, 1.9, -1.5, 1.5)
+dim = (3840, 5120)
+frac = Fractal('mandelbrot', 10000, dim, -0.73786069063659, -0.73753744616721, -0.17466298343187, -0.17442055007983)
 
-# el intento mamalon (8 horas)
 #frac = Fractal('mandelbrot', 8000, dim, -3.3 + 0.693333, 1.9 - 0.693333, -1.1, 1.1)
-
-# un julia set bien chido (pero no tan mamalon como el mandelbrot anterior)
 #frac = Fractal('julia', 8000, dim, -3.3 + 0.693333, 1.9 - 0.693333, -1.1, 1.1) 
 
 frac.generate() # genera el fractal
